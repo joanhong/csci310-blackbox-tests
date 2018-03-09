67a2382d79760be8e9d1daeda68d6115d0405620
Joan <joanhong@usc.edu>
2018-03-09 03:27:05 -0800

    latest

--- blackbox/features/step_definitions/prevcollage_steps.rb
+++ blackbox/features/step_definitions/prevcollage_steps.rb
@@ -29,23 +29,43 @@ And(/^the main "([^']*)" collage should be scaled down$/) do |arg1|
 end
 
 And(/^the clicked "([^']*)" collage should be scaled up$/) do
-  find('alt=' + arg1 + ']').native.css_value("width").should == "100%"
+  find('img[alt=' + arg1 + ']').native.css_value("width").should == "100%"
 end
 
 And(/^the collage picker should be at the bottom of the page$/) do
 end
 
 And(/^the "([^']*)" collage should not be displayed in the collage picker$/) do
-  
+  find('img[alt=' + arg1 + ']').native.css_value("width").should != "100px"
 end
 
-And(/^the collage picker should display scaled down images of previous collages$/) do 
+And(/^the collage picker should display scaled down images of previous collages$/) do
+  find('img[alt=vodka]').native.css_value("height").should == "100px"
 end
 
 #==========================================================================
 
 When(/^I create 10 collages$/) do 
-  
+  fill_in('searchtext', :with => "computer")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "fish")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "google")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "mountain")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "orange")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "red")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "rock")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "space")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "ucla")
+  click_button("searchbutton")
+  fill_in('searchtext', :with => "usc")
+  click_button("searchbutton")
 end
 
 Then(/^I should see a scroll bar on the previously created collages$/) do
